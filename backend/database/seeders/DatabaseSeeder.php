4819f331986bbff968aa657f9e88fec2b28f0397
Shoharu <evanadhiarjayohanes@gmail.com>
2025-09-11 13:45:11 +0700

    first commit for backend

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+<<<<<<< HEAD
         User::factory()->create([
             'name' => 'admin',
             'email' => 'admin@gmail.com',
