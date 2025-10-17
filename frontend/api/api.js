89791dedc44eb5e77fece17b2685a7bd7f86a8a0
RakaAditya3 <rakafury.ar@gmail.com>
2025-10-17 14:47:56 +0700

    fix

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -1,15 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-<<<<<<< HEAD
-  baseURL: "https://env-laravel.jh-beon.cloud/api",
-=======
   baseURL: "http://localhost:8000/api",
    headers: {
     "Accept": "application/json",
     "Content-Type": "application/json", // ✅ tambahkan ini
   },
->>>>>>> 5efe1eaa31e1aa6a090a2b5b1ba35ae0b65a78e2
 });
 
 api.interceptors.request.use((config) => {
